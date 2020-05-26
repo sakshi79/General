24713cba13d62274654f12a1ae8ef31ee0456140
Sakshi Bhatia <40262132+sakshi79@users.noreply.github.com>
2020-05-26 20:52:56 +0530

    Create Codefile.py

---
+++ Codefile.py
@@ -0,0 +1,15 @@
+#This is a code file to print fibonacci series.
+def fibonacci(n):
+    a = 0
+    b= 1
+    series_list = [a, b]
+    while len(series_list) <= n
+          i = a+b
+          series_list.append(i)
+          a = b
+          b = i
+    return series_list
+fibonacci(6)    
+
+
+
